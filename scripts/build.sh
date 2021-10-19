c395d79f569b52e00e247d60dc892fa6bc61c220
Kelvin <nguyen007@e.ntu.edu.sg>
2021-10-19 20:18:26 +0800

    🎉 Initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOCKER_ID=pebabion
+IMAGE=python-workspace
+
+
+if [ $# -eq 0 ]
+    then
+        tag="$DOCKER_ID/$IMAGE:latest"
+    else
+        tag="$DOCKER_ID/$IMAGE:$1"
+fi
+
+
+echo "$tag"
+
+docker build -t "$tag" --file "./docker/Dockerfile" "./docker"
+docker push $tag
